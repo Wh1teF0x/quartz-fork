b70360335d201038c5d641aeaff8af08511dbdfc
Wh1teF0x <untoxited@gmail.com>
2025-04-24 13:50:08 +0300

    feat: leaflet ready

--- quartz/components/scripts/leaflet.inline.ts
+++ quartz/components/scripts/leaflet.inline.ts
@@ -1,72 +1,84 @@
-import { parseYaml } from '../../util/parseYaml';
-import { FilePath, slugifyFilePath } from '../../util/path';
+import { parseYaml } from "../../util/parseYaml"
+import { FilePath, FullSlug, slugifyFilePath, transformLink } from "../../util/path"
 
 type LeafletProps = {
-  id: string;
-  height: string;
-  image: FilePath;
-  bounds: [[number, number], [number, number]];
-  defaultZoom?: number;
-  maxZoom?: number;
-  marker?: Array<string>;
-  minZoom?: number;
-  unit: string;
-};
+  id: string
+  height: string
+  image: FilePath
+  bounds: [[number, number], [number, number]]
+  defaultZoom?: number
+  maxZoom?: number
+  marker?: Array<string>
+  minZoom?: number
+  unit: string
+}
 
-document.addEventListener('nav', async () => {
-  const center = document.querySelector('.center') as HTMLElement;
-  const nodes = center.querySelectorAll('code.leaflet') as NodeListOf<HTMLElement>;
+function initMap(data: LeafletProps, currentSlug: FullSlug, allSlugs: Array<FullSlug>) {
+  const path = transformLink(currentSlug, data.image, { strategy: "shortest", allSlugs: allSlugs })
+  // const path = slugifyFilePath(data.image);
+  const posX = data.bounds[1][0] || 100
+  const posY = data.bounds[1][1] || 100
+  const map = window.L.map(data.id, {
+    attributionControl: false,
+    crs: window.L.CRS.Simple,
+  }).setView([posX / 2, posY / 2], data.defaultZoom)
+  map.createPane("base")
+  const image = window.L.imageOverlay(
+    path,
+    [
+      [0, 0],
+      [100, 150],
+    ],
+    { pane: "base" },
+  )
+  image.addTo(map)
+  return map
+}
 
-  if (!nodes) return;
-
-  const textMapping: WeakMap<HTMLElement, string> = new WeakMap();
-  for (const node of nodes) {
-    textMapping.set(node, node.innerText);
+function positionMarkers(
+  map: L.Map,
+  data: LeafletProps,
+  currentSlug: FullSlug,
+  allSlugs: Array<FullSlug>,
+) {
+  for (let marker of data?.marker || []) {
+    const [, py, px, link] = marker.split(",")
+    const absLink = transformLink(currentSlug, link, { strategy: "shortest", allSlugs: allSlugs })
+    window.L.marker([Number.parseInt(py, 10), Number.parseInt(px, 10)])
+      .addTo(map)
+      .bindPopup(`<a href=${absLink}>${link}</a>`)
   }
+}
+
+document.addEventListener("nav", async (e: CustomEventMap["nav"]) => {
+  const currentSlug = e.detail.url
+  const allSlugs = Object.keys(await fetchData) as Array<FullSlug>
+  const nodes = document.querySelectorAll(".center code.leaflet") as NodeListOf<HTMLElement>
+
+  if (!nodes) return
 
   async function renderLeaflet() {
     for (const node of nodes) {
-      const data = node.getAttribute('data-clipboard') as string;
+      const data = node.getAttribute("data-clipboard") as string
       if (!data) {
-        return;
+        return
       }
 
-      const formattedData = window.jsyaml.load(data) as string;
-      const jsonData = parseYaml<LeafletProps>(formattedData);
-      console.log('json data', jsonData);
+      const formattedData = window.jsyaml.load(data) as string
+      const jsonData = parseYaml<LeafletProps>(formattedData)
 
-      const leafletContainer = document.createElement('div');
-      leafletContainer.id = jsonData.id;
-      leafletContainer.style.height = jsonData.height;
+      const leafletContainer = document.createElement("div")
+      leafletContainer.id = jsonData.id
+      leafletContainer.style.height = jsonData.height
 
-      const parent = node.parentElement?.parentElement as HTMLElement;
-      parent.innerHTML = '';
-      parent.appendChild(leafletContainer);
+      const parent = node.parentElement as HTMLElement
+      parent.innerHTML = ""
+      parent.appendChild(leafletContainer)
 
-      const path = slugifyFilePath(jsonData.image);
-      const posX = jsonData.bounds[1][0] || 100;
-      const posY = jsonData.bounds[1][1] || 100;
-      const map = window.L.map(jsonData.id, { attributionControl: false }).setView(
-        [posX / 2, posY / 2],
-        jsonData.defaultZoom,
-      );
-      map.createPane('base');
-      const image = window.L.imageOverlay(path, jsonData.bounds, { pane: 'base' });
-      image.addTo(map);
-
-      for (let marker of jsonData?.marker || []) {
-        const [, px, py, link] = marker.split(',');
-        window.L.marker([Number.parseInt(px, 10), Number.parseInt(py, 10)])
-          .addTo(map)
-          .bindPopup(`<a href=${link}>${link}</a>`);
-      }
-      // TODO:
-      // объединить репы
-      // правильные ссылки
-      // ставить метки
-      // gitignore
+      const map = initMap(jsonData, currentSlug, allSlugs)
+      positionMarkers(map, jsonData, currentSlug, allSlugs)
     }
   }
 
-  await renderLeaflet();
-});
+  await renderLeaflet()
+})
